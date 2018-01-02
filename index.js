576c55f8d14f1684d2b9f303a7432e09c08bbc24
Douglas Joseph Pecot <djpecot@gmail.com>
2018-01-02 18:48:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ var height = 70
 // Don't worry about this
 module.exports = { name, height
 }
-
-if (name== "Susan"){
-  console.log("The name is ballin'")
-  }else if (name== "Joe"){
-  console.log("That name is super legit")
-}
